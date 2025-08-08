da5b5b1c443afd569f8458d21945bb38f27bd649
Vladislav <kalachinskii_vl@mail.ru>
2025-08-09 02:06:32 +0300

    Реализована 404

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -5,9 +5,12 @@ import styles from "./NotFound.module.css";
 export function NotFound() {
   return (
     <div className={styles.NotFound}>
+      <div className={styles.fon}>404</div>
       <h1>404</h1>
-      <h2>Страница не найдена</h2>
-      <Link to={ROUTES.HOME}>На главную</Link>
+      <h2 className={styles.text}>Страницы не существует</h2>
+      <Link className={styles.link} to={ROUTES.HERO}>
+        На главную
+      </Link>
     </div>
   );
 }
