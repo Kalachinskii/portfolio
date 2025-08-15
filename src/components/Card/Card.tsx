6bc1f1fa1efcabc1644bab67b619c703074c6c04
Vladislav <kalachinskii_vl@mail.ru>
2025-08-16 00:23:38 +0300

    fix: style card 2

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -17,22 +17,10 @@ export function Card({
   return (
     <div className={styles.cardBox}>
       <img className={styles.img} src={imageUrl} alt="Изображение проекта" />
-      {/* <div className={styles.iconBox}>
-        <span className={styles.fonIcon}></span>
-        <i
-          className={`${styles.icon} ${styles.steckIcon} fa-solid fa-gears`}
-        ></i>
-        <i
-          className={`${styles.icon} ${styles.linkIcon} fa-solid fa-globe`}
-        ></i>
-        <i
-          className={`${styles.icon} ${styles.gitIcon} fa-brands fa-github`}
-        ></i>
-      </div> */}
-      <div className={styles.contentBox}>
-        <Texnologys technologies={technologies} />
-        <Links links={links} />
-      </div>
+      {/* <div className={styles.contentBox}> */}
+      <Texnologys technologies={technologies} />
+      <Links links={links} />
+      {/* </div> */}
     </div>
   );
 }
