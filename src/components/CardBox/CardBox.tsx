5c30e93457cde55c12a0c3f4dab75839f56df8e0
Vladislav <kalachinskii_vl@mail.ru>
2025-08-17 22:30:54 +0300

    fix: исправление карточек

--- src/components/CardBox/CardBox.tsx
+++ src/components/CardBox/CardBox.tsx
@@ -1,8 +1,18 @@
 import { Card } from "../Card/Card";
 import json from "../../assets/projects.json";
 import styles from "./CardBox.module.css";
+import { div } from "framer-motion/client";
+import { color } from "framer-motion";
 
 export const CardBox = ({ sort }: { sort?: string }) => {
+  const testStyle = {
+    color: "red",
+    border: "1px solid #00fe00ed",
+    width: "150px",
+    height: "150px",
+    background: "#000",
+  };
+
   const filteredProjects = sort
     ? json.filter((project) =>
         project.technologies.some(
@@ -14,7 +24,9 @@ export const CardBox = ({ sort }: { sort?: string }) => {
   return (
     <div className={styles.cardsBox}>
       {filteredProjects.map((cardProps) => (
-        <Card key={cardProps.id} {...cardProps} />
+        // <Card key={cardProps.id} {...cardProps} />
+        // <div style={testStyle}></div>
+        <div style={testStyle}></div>
       ))}
     </div>
   );
--- src/pages/Works/Works.tsx
+++ src/pages/Works/Works.tsx
@@ -1,5 +1,7 @@
 import { NavLink, Outlet } from "react-router-dom";
 import styles from "./Works.module.css";
+import { Links } from "../../components/Links/Links";
+import json from "../../assets/projects.json";
 
 export function Works() {
   const navigation = [
@@ -10,11 +12,39 @@ export function Works() {
   ];
 
   return (
-    <div className={styles.worksConteiner}>
-      <div className={styles.box}>
+    <div className={`${styles.worksConteiner}`}>
+      <div className={`${styles.box} ${styles.imageCover}`}>
+        <div className={`${styles.circle}`}>
+          <div className={`${styles.leftConteiner}`}>
+            <h1 className={`${styles.title}`}>Red explorers</h1>
+            <p className={`${styles.about}`}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+              voluptatum eos. Rerum saepe iusto veniam eaque neque iure, ratione
+              minima placeat voluptates impedit sequi aut ipsum soluta ex
+              assumenda quam.
+            </p>
+            <div className={styles.box}>
+              {json[1].technologies.map((name) => (
+                <span key={name} className={styles.texnology}>
+                  {name}
+                </span>
+              ))}
+            </div>
+            <div className={styles.linkBox}>
+              <Links iconName="fa-brands fa-github" href={"#"}>
+                Гит
+              </Links>
+              <Links iconName="fa-solid fa-globe" href={"#"}>
+                Сайт
+              </Links>
+            </div>
+          </div>
+        </div>
+        <div className={`${styles.rightConteiner}`}></div>
+
         {/* <h1>Мои работы</h1> */}
-        <div className={styles.navigationLink}>
-          {navigation.map((nav, ind) => (
+        {/* <div className={styles.navigationLink}> */}
+        {/* {navigation.map((nav, ind) => (
             <NavLink
               className={({ isActive }) =>
                 `${styles.link} ${isActive ? styles.active : ""}`
@@ -24,11 +54,9 @@ export function Works() {
             >
               {nav.name}
             </NavLink>
-          ))}
-        </div>
-        <div className={styles.contentBox}>
-          <Outlet />
-        </div>
+          ))} */}
+        {/* </div> */}
+        {/* <div className={styles.contentBox}>{<Outlet />}</div> */}
       </div>
     </div>
   );
