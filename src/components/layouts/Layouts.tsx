fe85fcf4060d3ed55a7e24f9f29d25d4d2535464
Vladislav <kalachinskii_vl@mail.ru>
2025-10-02 00:51:20 +0300

    fix: works убради фоновое видео - нет необходимости

--- src/components/layouts/Layouts.tsx
+++ src/components/layouts/Layouts.tsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Cover from "../Cover/Cover";
 import { Nav } from "../Nav/Nav";
 
 export function Layouts() {
+  const location = useLocation();
+  const isProjectsPage = location.pathname.includes("/projects");
+
   return (
     <>
       <Nav />
-      {/* <SocialLinks /> */}
-      <Cover />
+      {/* убераем видео фон для проектов т.к. Image всеравно его перекроет */}
+      {!isProjectsPage && <Cover />}
       <Outlet />
     </>
   );
